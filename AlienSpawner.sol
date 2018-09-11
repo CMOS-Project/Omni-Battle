3187146e50b3037d27f7bcc7812c607828b95b10
OMR Developer <cmosproj@gmail.com>
2018-09-11 23:45:19 +0100

    AlienOwnership.sol

--- AlienSpawner.sol
+++ AlienSpawner.sol
@@ -3,15 +3,47 @@ pragma solidity ^0.4.16;
 import "./AlienOwnership.sol";
 import "./safemath.sol";
 import "./ERC721.sol";
+import "./interest.sol";
 
+/// #CONTRACT Create Core Base
+contract CreateCoreBase is interest, safemath {
 
+    event GenerateCore(uint hp, uint value, uint CoreId);
+
+    struct CoreBase{
+    uint hp;
+    uint RegenRate;
+    uint interest;
+    uint value;
+    string Aliens;
+    uint AlienCount;
+    uint LossCounter;
+    uint WinCounter;
+}
+
+CoreBase[] Core;
+
+mapping (address => uint) CoreBaseOwner; ///#DEV Find which CoreBase Id belongs to corresponding ETH address
+
+function GenerateCoreBase(_hp, _RegenRate, _interest, _value, _AlienCount, _LossCounter, _WinCounter) private {
+    address owner = msg.sender;
+    require(balanceOf(msg.sender) < 1 );
+    CoreBase.AlienCount = balanceOf(msg.sender);
+    uint CoreId = CoreBase.push(Core(100, 1 days, 0, 0, Aliens[],  _AlienCount, 0, 0) - 1);
+    CoreBaseOwner(_CoreId);
+    GenerateCore(_hp, _value, _Core);
+}
+
+}
 
 /// #CONTRACT Create the Aliens contract
-contract AlienSpawner is SafeMath, ERC721 {
+contract AlienSpawner is SafeMath, ERC721, CreateCoreBase {
+
+    event CreateAlien(uint Alienstrand, uint name, uint AlienId);
 
 
     uint Combos = 20;
-    uint Total = 20 ** Combos ; /// #DEV Total combos of Alien type 1
+    uint Total = 20 ** Combos; /// #DEV Total combos of Alien type 1
     uint HybridCooldown = 1 days;
 
 
@@ -26,40 +58,34 @@ contract AlienSpawner is SafeMath, ERC721 {
         uint64 AmalgamationCooldown;
     }
 
-
-struct Armor{
-    uint defence;
-    uint durability;
-    uint64 DefenceCooldown;
-}
-
-struct Weapons{
-    uint attack;
-}
-
-struct CoreBase{
-    uint hp;
-    uint RegenRate;
-}
-
 AlienType[] alien; ///#DEV Aliens array
 
-function CreateRandomAlien() private {
-    alien.push(AlienType[_name, _AlienSub, _attack, _defence, 5, 5, uint64(_AttackCooldown + _AmalgamationCooldown)] - 1); ///#DEV Add alien (Name, attack score, defence score, level, armor boost, cooldown between attacks, hybrid creation, start at index 0)
+mapping(address => uint) AlienOwner; //#DEV Find Aliens owned by ETH address (Key = ETH Address & Value = id of Alien)
+
+address owner = msg.sender;
+
+function CreateRandomAlien(_name, _AlienSub, _attack, _defence, _level, _armor, _AttackCooldown, _AmalgamationCooldown) private {
+    uint AlienId = alien.push(AlienType(_name, _AlienSub, _attack, _defence, 5, 5, uint64(_AttackCooldown + _AmalgamationCooldown)) - 1); ///#DEV Add alien (Name, attack score, defence score, level, armor boost, cooldown between attacks, hybrid creation, start at index 0)
+    AlienOwner[_AlienId];
+    CreateAlien(_strand, _name, _tokenId);
 }
 
 string _str;
 
-function GetAlienRand() private returns(uint) {
-    require(balanceOf(msg.sender) <= 3);
-    return  _str == keccak256(_str);
 
-    uint (msg.sender) == uint(keccak256(_str));/// #DEV First time users can generate 3 aliens to start off with
+///#DEV Better to create separate card type with ERC721X out. Keeping for now.
+function GetAlienRand(_owner, _AlienId) internal returns(uint) {
+    require(balanceOf(_owner, _AlienId) < 3);
+    return  _str == keccak256(_str);
+     /// #DEV First time users can generate 3 aliens to start off with
      /// #DEV Generates a random 256 bit string
 }
 
+      ///uint(msg.sender) == uint(keccak256(_str));
 
-    function TypeOfAlien(uint _defence, uint _attack, string _AlienType) private  {
+
+    function TypeOfAlien(uint _defence, uint _attack, string _AlienType) private {
+        CreateRandomAlien(_strand, _Aliensub, _name, _attack, _defence, _AttackCooldown);
         GetAlienRand();
 
         uint counter_x = 0;
@@ -75,43 +101,146 @@ function GetAlienRand() private returns(uint) {
         }
 
     if(counter_x > 7 && counter_y <= 8  ) { /// #DEV If more than 15 Xs
-      _defence = _defence + add(5);  /// #DEV Safe math. Adds +5 defence
-      _AlienSub = keccak256("Anorak");  /// #DEV Higher than average defence
+      AlienType.defence = _AlienType.defence + add(5);  /// #DEV Safe math. Adds +5 defence
+      AlienType.AlienSub = keccak256("Anorak");  /// #DEV Higher than average defence
     }
 
     else if(counter_y > 7 && counter_x <= 8) {
-      _attack = _attack + add(5);        /// #DEV Safe math. Adds +5 attack
-      _AlienSub = keccak256("Varg");        /// #DEV If more than 15 Ys
+      AlienType.attack = AlienType._attack + add(5);        /// #DEV Safe math. Adds +5 attack
+      AlienType.AlienSub = keccak256("Varg");        /// #DEV If more than 15 Ys
                                        /// #DEV Higher than average attack
     }
 
-    else if(counter_x > 7 && counter_y <= 8) {
-       _defence = _defence + add(5);
-       _attack = _attack + add(5);               /// #DEV
-       _AlienSub = keccak256("Zorg");/// #DEV If more than 15Xs and 15Ys
+    else if(counter_x > 7 && counter_y > 7) {
+     AlienType.defence = AlienType._defence + add(5);
+     AlienType.attack = AlienType._attack + add(5);   /// #DEV
+     AlienType.AlienSub = keccak256("Zorg");/// #DEV If more than 15Xs and 15Ys
                                  /// #DEV All rounded
     }
 
     else {
-        _AlienType = keccak256("Regular");
-        /// #DEV Common Starter
+        AlienType.AlienSub = keccak256("Regular"); /// #DEV Common Starter
+    }
+}
+}
+/// Assign newly created alien to the base
+
+   function AssignAlienCoreBase(_AlienId) internal {
+    require(msg.sender == OwnerOf(_AlienId)); /// #DEV Must be the owner of alien
+    require(msg.sender == OwnerOf(_CoreId)); ///#DEV Must own the core base
+    CoreBase.Aliens.push(_AlienId); /// #DEV Add Zombie to the core base
+    CoreBase._AlienNum = CoreBase._AlienNum + add(1); /// #DEV Alien count increases by 1
+}
+
+}
+
+contract ArmorSpawner is AlienSpawner, CreateCoreBase {
+
+    event CreateArmors(uint armorId, uint Type, uint defence);
+    event ArmorUsed(uint armorId, uint used);
+
+    struct Armor{
+    string ArmorType;
+    uint defence;
+    uint used;
     }
 
-    CreateRandomAlien(_AlienNum, _name, _attack, _defence, _AttackCooldown, _sub);
+    mapping(address => uint) ArmorOwner;
+
+     Armor[] armors;
+
+
+    function CheckAlienIdBase(_CoreId, _AlienId) internal view returns (bool) {
+        if(OwnerOf(_CoreId) = OwnerOf(_AlienId)) {
+           return true;
+        }
+
+        else {
+          return false;
+        }
+    }
+
+    function CheckArmorPresent() internal view returns (bool) {
+        if(AlienType.armor > 0) {
+          return true;
+        }
+
+        else {
+          return false;
+        }
+    }
+
+    function CreateArmor() internal {
+        address owner = msg.sender;
+        require(CheckArmorPresent = false);
+        uint armorId = armors.push("Regular", 10 );
+        ArmorOwner[_ArmorId];
+        CreateArmor(_Type, _defence);
+        ArmorUsed(_armorId, _used );
+    }
+
+    function CheckArmorUsed() internal view returns (bool) {
+        if(Armor.used = true) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    function AssignArmor(_Alien_id) internal {
+        require(CheckAlienIdBase() = true);
+        require(msg.sender == OwnerOf(_AlienId));
+        require(AlienType.level > 1);
+        require(CheckArmorUsed() = false);
+        CreateArmor();
+        AlienType.defence = AlienType.defence + add(10);
+        delete armorId;
+    }
+
 }
 
+contract weapons is AlienSpawner, CreateCoreBase {
 
+    event WeaponCreation(uint weaponId, uint attack);
 
+    uint Combos = 16;
+    uint Total = 16 ** Combos;
 
+    struct weapons {
+        uint attack;
+        uint occupied;
+        uint cooldown;
+    }
 
+    weapons[] Weapons;
 
-}
+    function CreateWeapons() internal {
+       require(CheckAlienIdBase = true);
+       uint weaponId = weapons.push(Weapons(_attack, _occupied));
+       WeaponCreation(_weaponId, _attack);
+    }
 
+    function CheckWeaponsPresent() internal view returns (bool) {
+        if(weapons.occupied = true) {
+            return true;
+        }
 
+        else {
+            return false;
+        }
+    }
 
-
-
-
+    function AssignWeapons(_AlienId) internal {
+        require(CheckAlienIdBase() = true);
+        require(msg.sender == OwnerOf(_tokenId));
+        require(msg.sender == OwnerOf(_weaponId));
+        require(AlienType.level > 1);
+        require(CheckWeaponsPresent() = false);
+        CreateWeapons();
+        AlienType.attack = AlienType.attack + add(10);
+        delete _weaponId;
+    }
 
 
 
--- Interest.sol
+++ Interest.sol
@@ -1,51 +1,113 @@
-pragma solidity ^0.4.16;      
+pragma solidity ^0.4.16;
 
-import "./browser/draft.sol";
-import "./ERC721";
+import "./ERC721.sol";
+import "./AlienSpawner.sol";
 
-contract InterestExample is BrowserDraft, ERC721 {
+contract InterestCurrency {
 
-function AccruedInterest(_Nebuli , uint time) private returns {
-    if(now >= _time + 1 weeks && CoreBase.hp = 100) {
-        require(msg.sender = _Nebuli) ///#DEV Check if person with NBL address matches request to claim interest
-       uint week1 = 1.001 * balanceOf(_Nebuli)
-    return transfer(_Nebuli, _week1) /// #DEV Returns 0.1% interest
+       event VirtualBalance(uint OmniGold, uint OmniGoldId);
+
+       mapping(address => uint) OmniGoldOwner;
+
+    function VirtualPrinter(uint OmniGold) internal { ///#DEV Creation of ERC 721 token used exclusively for locking in value of the base
+        address owner = msg.sender;
+        uint OmniGoldId = OmniGold;
+        OmniGoldOwner(_OmniGoldId);
+        VirtualBalance(_OmniGold, _OmniGoldId);
+    }
+
+function AccruedInterest() internal {
+    uint number = 0;
+    numb[];
+
+    if(now >= 1 weeks && CoreBase.hp = 100) {
+        require(msg.sender == OwnerOf(_CoreId)); ///#DEV Check if owner of tokenmatches request to claim interest
+        numb.push(1);
+        CoreBase.interest++;
+        VirtualPrinter(_OmniGold);
+     /// #DEV Returns 1 new card
     }
 
     else if(now >= _time + 2 weeks && CoreBase.hp = 100) {
-        require(msg.sender = _Nebuli)
-        uint week2 = 1.002 * balanceOf(_Nebuli)
-        return transfer(_Nebuli, _week2)
+        require(msg.sender == OwnerOf(_CoreId));
+        numb.push(2);
+        for(uint i = 0; uint < numb.length; i = i + add(1)){
+           CoreBase.interest++;
+           VirtualPrinter(_OmniGold);
+        }
     }
 
     else if(now >= _time + 3 weeks && CoreBase.hp = 100) {
-        require(msg.sender = _Nebuli)
-        uint week3 = 1.003 * balanceOf(_Nebuli)
-    return transfer(_Nebuli, _week3)
+        require(msg.sender == OwnerOf(_CoreId));
+        numb.push(3);
+        for(i = 0; uint < numb.length; i = i + add(1)){
+            CoreBase.interest++;
+            VirtualPrinter(_OmniGold);
+        }
     }
 
     else if(now >= _time + 4 weeks && CoreBase.hp = 100) {
-        require(msg.sender = _Nebuli)
-        uint week4 = 1.004 * balanceOf(_Nebuli)
-    return transfer(_Nebuli, _week4)
+        require(msg.sender == OwnerOf(_CoreId));
+        numb.push(4);
+        for(i = 0; uint < numb.length; i = i + add(1)){
+            CoreBase.interest++;
+            VirtualPrinter(_OmniGold);
+        }
     }
 
     else if(now >= _time + 5 weeks && CoreBase.hp = 100) {
-        require(msg.sender = _Nebuli)
-        uint week5 = 1.005 * balanceOf(_Nebuli)
-    return transfer(_Nebuli, _week5)
+        require(msg.sender == OwnerOf(_CoreId));
+        numb.push(5);
+        for(i = 0; uint < numb.length; i = i + add(1)){
+            CoreBase.interest++;
+            VirtualPrinter(_OmniGold);
+    }
     }
 
     else if(now >= _time + 6 weeks && CoreBase.hp = 100) {
-        require(msg.sender = _Nebuli)
-        uint week6 = 1.006 * balanceOf(_Nebuli)
-    return transfer(_Nebuli, _week6)
+        numb.push(6);
+    for(i = 0; uint < numb.length; i = i + add(1)){
+        CoreBase.interest++;
+    }
     }
 
-     else {
-         require(msg.sender = _Nebuli)
-         _time = 0 days
-
+    else {
+        delete numb;
     }
 }
+
+function CheckVal() internal view returns (bool) {   ///#DEV Value is equal to 1
+   if(VirtualPrinter.Val = 1) {
+    return true; /// #DEV Should return true
+    }
+    else {
+    return false;
+    }
 }
+
+function RedeemOmniGold(_OmniGoldId, _AlienId) internal {
+    require(msg.sender == OwnerOf(_AlienId));
+    require(msg.sender == OwnerOf(_OmniGoldId));
+    CheckVal() = true;
+    CreateRandomAlien(_strand, _Aliensub, _name, _attack, _defence, _AttackCooldown);
+    delete _OmniGoldId; /// #DEV Can only be used once. Value reset to 0 afterwards.
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
