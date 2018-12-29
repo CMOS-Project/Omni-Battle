3a18b2f91d30ff4274c035abe2f622b647af7334
OMR Developer <cmosproj@gmail.com>
2018-12-29 02:50:39 +0000

    Alien Ownership modifiers

---
+++ contracts/AlienOwnership.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.24;
+
+import "./AlienSpawner.sol";
+
+contract weapon is AlienCreation {
+    /*function AddWeaponry(uint _attack, uint _bonus, uint _tokenId) external TokenOwner(_tokenId) {
+        //Alien storage alien = AlienAddress[_tokenId];
+        AlienAddress[_tokenId].attack;
+    }*/
+}
