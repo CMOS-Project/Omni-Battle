22c8a41e67c10e5a1f8f98af7fb221f419910d85
OMR Developer <cmosproj@gmail.com>
2019-01-19 21:54:04 +0000

    Will be used for key token upgrades

---
+++ contracts/Upgrades.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.24;
+
+import "./AlienSpawner.sol";
+
+/*contract Upgrade is PlanetCreation {
+
+
+function UpgradeCheck(uint _planetId) pure returns (uint) {
+  return planet[_planetId].tokenCount
+}
+
+function UpgradeCapacity(uint _planetId) internal payable PlanetOwnerCheck(_planetId)  {
+  require(msg.value == 0.001 ether, "0.001 ETH must be transferred");
+  require(UpgradeCheck(_planetId) == planet[_planetId].capacity));
+   planet[_planetId].capacity = 200;
+   planet[_planetId].level++;
+}
+
+}*/
