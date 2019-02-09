341e00ee5744a1e96197344ea6847ba9c01237f2
OMR Developer <cmosproj@gmail.com>
2019-02-09 21:58:22 +0000

    Upgrade weapons + add capacity to planet

--- contracts/Upgrades.sol
+++ contracts/Upgrades.sol
@@ -2,18 +2,51 @@ pragma solidity ^0.4.24;
 
 import "./AlienSpawner.sol";
 
-/*contract Upgrade is PlanetCreation {
+contract Upgrade is AlienCreation {
 
 
-function UpgradeCheck(uint _planetId) pure returns (uint) {
-  return planet[_planetId].tokenCount
+function UpgradeCheck(uint _planetId) public view returns (uint) {
+  return planet[_planetId].tokenCount;
 }
 
-function UpgradeCapacity(uint _planetId) internal payable PlanetOwnerCheck(_planetId)  {
+function UpgradeCapacity(uint _planetId) public payable planetOwnerCheck(_planetId)  {
   require(msg.value == 0.001 ether, "0.001 ETH must be transferred");
-  require(UpgradeCheck(_planetId) == planet[_planetId].capacity));
-   planet[_planetId].capacity = 200;
+  uint tokenCount = UpgradeCheck(_planetId);
+  if(tokenCount == 100) {
+   planet[_planetId].capacity = 250;
    planet[_planetId].level++;
 }
 
-}*/
+else if(tokenCount == 250) {
+  planet[_planetId].capacity = 500;
+  planet[_planetId].level++;
+}
+
+else if(tokenCount == 500) {
+  planet[_planetId].capacity = 1000;
+  planet[_planetId].level++;
+}
+
+else if(tokenCount == 1000) {
+  planet[_planetId].capacity = 2500;
+  planet[_planetId].level++;
+}
+
+else if(tokenCount == 2500) {
+  planet[_planetId].capacity = 5000;
+  planet[_planetId].level++;
+}
+
+else {
+  revert("Full capacity has not been reached");
+}
+
+}
+
+function UpgradeWeaponry(uint _alienId, uint _weaponId) public payable OnlyAlienOwner(_alienId) CheckWeaponMerge(_weaponId, _alienId) {
+require(msg.value == 0.0001 ether, "0.0001 ETH must be transferred");
+weapon[_weaponId].attack++;
+alien[_alienId].attack++;
+}
+
+}
