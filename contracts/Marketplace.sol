c2e3750e6c7761147ac9e4b5a7e6aad9acf96888
OMR Developer <cmosproj@gmail.com>
2019-02-11 23:31:51 +0000

    Add transfer events, ether transfer method and send failed leveraged auctions to burn address

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -5,55 +5,142 @@ import "./Upgrades.sol";
 
 contract Marketplace is Upgrade {
 
+  event listLeverage(uint startingBid, bool instabuyoption, bool leverage, uint alienId);
+  event listNonLeverage(uint startingBidN, bool instabuyoptionN, bool leverageN, uint alienIdN);
+
   struct Auction {
    address oldOwner;
-   uint minValue;
-   uint tokenId;
+   uint value;
    uint auctionId;
+   uint bidCount;
+   uint8 time;
    bool instabuyoption;
    bool leverage;
+   uint leverageSelector;
+   bool ended;
   }
 
   Auction[] auction;
 
-  mapping(address => address) public transactionParticipants;
+  mapping(address => address) public highestBidder;
   mapping(address => uint) public leverageInitiate;
   mapping(address => uint) public nonLeverageInitiate;
   mapping(uint => uint) public alienToAuction;
 
 
+function randomAuctionId() private view returns (uint) {
+  uint rand = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, blockhash(block.number))));
+  return rand;
+}
 
-  function ListCard(uint _startingBid, bool _instabuyoption, bool _leverage, uint _tokenId, uint _auctionId, uint _alienId) public OnlyAlienOwner(_alienId) {
+
+  function ListCard(uint _startingBid, bool _instabuyoption, bool _leverage, uint _alienId, uint _leverageSelector) external OnlyAlienOwner(_alienId) {
     require(alien[_alienId].level >= 1);
     require(_startingBid >= 0.001 ether); ///#DEV Min bid of 0.001 ether
     require(_instabuyoption == true);
+    require(_leverageSelector <= 5);
 
-    _tokenId = alien[_alienId].alienId;
+    uint8 _time = uint8(now + 30 days);
+    uint _auctionId = randomAuctionId();
 
     if(_leverage == true) {
       leverageInitiate[msg.sender] = _alienId;
-      auction.push(Auction(msg.sender, _startingBid, _tokenId, _auctionId, true, true));
+      alienToAuction[_auctionId] = leverageInitiate[msg.sender];
+      auction.push(Auction(msg.sender, _startingBid, _auctionId, 0, _time, true, true, _leverageSelector, false));
+      emit listLeverage(_startingBid, _instabuyoption, _leverage, _alienId);
     }
 
     else {
       nonLeverageInitiate[msg.sender] = _alienId;
-      auction.push(Auction(msg.sender, _startingBid, _tokenId, _auctionId, true, false));
+      auction.push(Auction(msg.sender, _startingBid, _auctionId, 0, _time,  true, false, 0, false));
+      emit listNonLeverage(_startingBid, _instabuyoption, _leverage, _alienId);
     }
 
     alienToAuction[_alienId] = _auctionId;
   }
 
 
-  function PlaceBid(/*uint _alienId,*/ uint _auctionId) public payable {
-    require(msg.value >= auction[_auctionId].minValue);
+  function PlaceBid(uint _auctionId, uint _alienId) external payable {
+    require(msg.value > auction[_auctionId].value);
     require(msg.sender != auction[_auctionId].oldOwner);
-    transactionParticipants[auction[_auctionId].oldOwner] = msg.sender;
+    require(AlienOwner[_alienId].alienOwner != msg.sender);
+    require(auction[_auctionId].ended == false);
+    highestBidder[auction[_auctionId].oldOwner] = msg.sender;
+    auction[_auctionId].value = msg.value;
+    auction[_auctionId].bidCount++;
   }
 
-  //function StartAuction
-  /*function EndAuction() internal payable {
+  function leverageChances(uint _auctionId) view public returns (uint) {
+      uint leverageSelector = auction[_auctionId].leverageSelector;
+      uint chances = 100;
 
-  }*/
+      for(uint i = 1; i < leverageSelector;i++) {
+        chances -=20;
+      }
+
+      return chances;
+  }
+
+  function leverageRandom() view public returns (uint) {
+    uint rand = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender))) % 100;
+    return rand;
+  }
+
+
+
+  function leverageResult(uint _auctionId) view public returns (bool) {
+     uint chance = leverageChances(_auctionId);
+     uint rand = leverageRandom();
+
+     if(chance > rand) {
+       return true;
+     }
+
+     else {
+       return false;
+     }
+
+  }
+  //function nonLeverageResult()
+
+  function AuctionEnds(uint _auctionId) external payable {
+    require(msg.value >= auction[_auctionId].value, "Ether sent must be equal to or of more value than the final bid amount. No additional Ether sent is returned.");
+    require(highestBidder[auction[_auctionId].oldOwner] ==  msg.sender, "Only the final bidder can call this");
+    require(now >= auction[_auctionId].time, "The auction must have ended");
+
+    uint alienId = alienToAuction[_auctionId];
+    uint transferToBidder = auction[_auctionId].value * 1000000000000000000;
+    auction[_auctionId].oldOwner.transfer(transferToBidder);
+
+    if(auction[_auctionId].leverage == true) {
+       bool leverageRes = leverageResult(_auctionId);
+
+       if(leverageRes == true) {
+        _mint(alienId, highestBidder[auction[_auctionId].oldOwner], auction[_auctionId].leverageSelector);
+        transferFrom(auction[_auctionId].oldOwner, highestBidder[auction[_auctionId].oldOwner], alienId, 1);
+        emit TransferWithQuantity(auction[_auctionId].oldOwner, highestBidder[auction[_auctionId].oldOwner], alienId, 1);
+        //emit transfer(auction[_auctionId].oldOwner, alien[_alienId]);
+       }
+
+       else if(leverageRes == false) {
+        transferFrom(auction[_auctionId].oldOwner, 0x0000000000000000000000000000000000000000, alienId, 1);
+        emit TransferWithQuantity(auction[_auctionId].oldOwner, 0x0000000000000000000000000000000000000000 , alienId, 1);
+       }
+
+       else {
+         revert();
+       }
+    }
+
+    else if(auction[_auctionId].leverage == false) {
+      transferFrom(auction[_auctionId].oldOwner, highestBidder[auction[_auctionId].oldOwner], alienId , 1);
+      emit TransferWithQuantity(auction[_auctionId].oldOwner, highestBidder[auction[_auctionId].oldOwner], alienId, 1);
+    }
+
+    else {
+      revert();
+    }
+  }
 
 
   function SpecsLeverageBid(uint _alienId) external view returns (uint[3]) {  ///#DEV No way to return object in solidity yet
